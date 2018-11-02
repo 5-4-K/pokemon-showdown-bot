94799c6c935787cabfc0e68b9db26f06a8b4d591
Shafi Al Kader <saksopan@gmail.com>
2018-11-02 22:06:50 +0600

    bd ou theorymon team for bot

--- features/battle/teams.js
+++ features/battle/teams.js
@@ -3,5 +3,5 @@
 */
 
 exports.teams = {
-	//Example: "tier": ['team1', 'team2']
+	"gen7bdoutheorymon": ['|mismagius|moonball||shadowball,moonblast,mysticalfire,nastyplot|Timid|,,,252,4,252|F|,0,,,,|||]|greninjaash|choicespecs||hydropump,darkpulse,watershuriken,spikes|Timid|,,,252,4,252|||||]|landorustherian|rockiumz||earthquake,stoneedge,swordsdance,stealthrock|Adamant|,252,4,,,252|||||]|celesteela|leftovers||heavyslam,flamethrower,leechseed,protect|Sassy|248,,12,,248,|||||]|mew|leftovers||icebeam,willowisp,defog,softboiled|Timid|248,,112,,36,112||,0,,,,|||]|tsareena|choicescarf|1|tropkick,playrough,highjumpkick,uturn|Jolly|,252,,,4,252|||||']
 };
